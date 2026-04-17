d967ca27cf83e93bdc79d7295e612b5c2f16d406
花开半夏有落时 <134383324+shuzhanboke@users.noreply.github.com>
2026-04-17 19:05:00 +0800

    fix: correct next.config.js syntax

--- next.config.js
+++ next.config.js
@@ -4,9 +4,10 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '*',
+        hostname: '**',
       },
     ],
+  },
 }
 
 module.exports = nextConfig
